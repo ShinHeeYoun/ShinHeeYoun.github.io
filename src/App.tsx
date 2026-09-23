3cd03d82555aeb013c94b2e78f014bf8b5e3bd7f
ShinHeeYoun <eustaph7725@gmail.com>
2026-09-23 09:44:29 +0900

    feat: add shared navigation bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import NavBar from './components/NavBar'
 import Home from './pages/Home'
 import Blog from './pages/Blog'
 import PostDetail from './pages/PostDetail'
@@ -6,11 +7,14 @@ import Write from './pages/Write'
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/blog" element={<Blog />} />
-      <Route path="/blog/:slug" element={<PostDetail />} />
-      <Route path="/write" element={<Write />} />
-    </Routes>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/blog/:slug" element={<PostDetail />} />
+        <Route path="/write" element={<Write />} />
+      </Routes>
+    </>
   )
 }
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom'
+
+export default function NavBar() {
+  return (
+    <nav className="border-b border-gray-200">
+      <div className="mx-auto flex max-w-2xl gap-6 px-4 py-4">
+        <Link to="/" className="font-semibold">
+          Home
+        </Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/write">Write</Link>
+      </div>
+    </nav>
+  )
+}
